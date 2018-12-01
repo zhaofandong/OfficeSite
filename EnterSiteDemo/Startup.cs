7a88b7276c4781118a7bf7e2fb82d096f67f4c2d
zhaofandong <zhao_f_x@163.com>
2018-12-01 10:40:53 +0800

    添加项目文件。

---
+++ EnterSiteDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnterSiteDemo.Startup))]
+namespace EnterSiteDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
